4cc483ab295307e5492ff6acb32b20e7d0169c93
mustakmiraj5 <wildmiraj@gmail.com>
2024-01-19 19:23:46 +0600

    note

---
+++ notes.py
@@ -0,0 +1,21 @@
+def f(i):
+    return [i]
+
+print(f(2))
+
+str = ["eat","tea","tan","ate","nat","bat"]
+s = "tan"
+if s in str:
+    print("True")
+xMap = {}
+# print(len(xMap))
+
+nums = [0,0,1,1,1,2,2,3,3,4]
+
+for i in range(len(nums)):
+    xMap[nums[i]] = 1 + xMap.get(nums[i], 0)
+print(len(xMap))
+print(xMap.keys())
+
+x = [3,1,4]
+print(sorted(x))
